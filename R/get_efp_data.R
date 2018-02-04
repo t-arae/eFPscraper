ebd117aef78e05b3f1318b61d5b6419508fc1f9d
Toshihiro Arae <aroearue@gmail.com>
2018-02-04 14:03:47 +0900

    feat: add data DATASOURCE

--- R/get_efp_data.R
+++ R/get_efp_data.R
@@ -1,26 +1,5 @@
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
-DATASOURCE <-
-  c(
-    "Abiotic_Stress", #1
-    "Abiotic_Stress_II", #2
-    "Biotic_Stress", #3
-    "Biotic_Stress_II", #4
-    "Chemical", #5
-    "Developmental_Map", #6
-    "Developmental_Mutants", #7
-    "Guard_Cell", #8
-    "Hormone", #9
-    "Lateral_Root_Initiation", #10
-    "Light_Series", #11
-    "Natural_Variation", #12
-    "Regeneration", #13
-    "Root", #14
-    "Root_II", #15
-    "Seed", #16
-    "Tissue_Specific" #17
-  )
-
 #' Get eFP browser data
 #' @importFrom xml2 read_html
 #' @importFrom magrittr %>%
